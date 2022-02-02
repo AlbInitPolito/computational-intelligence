4e94068c557392890c3793b1ce39e3dcdd37c6a6
Alberto Castrignanò <82368828+AlbInitPolito@users.noreply.github.com>
2022-02-02 17:10:08 +0100

    add try2

---
+++ project/hanabi/try2.py
@@ -0,0 +1,40 @@
+import checks
+import numpy as np
+import Qprocess as qp
+
+class card:
+    def __init__(self, id, value, color):
+        self.id = id
+        self.value = value
+        self.color = color
+
+albo_hand = [card(0,0,None),card(0,0,None),card(0,0,None),card(0,0,None),card(0,0,None)]
+steo_hand = [card(0,0,None),card(0,0,None),card(0,0,None),card(0,0,None),card(0,0,None)]
+tableCards = {
+          'red': [],
+          'blue': [],
+          'yellow': [],
+          'green': [],
+          'white': [],
+          }
+
+discardPile = []
+
+class player:
+    def __init__(self, name, hand):
+        self.name = name
+        self.hand = hand
+
+players = [ player('albo',[]), player('steo',steo_hand) ]
+
+class state:
+    def __init__(self, tableCards, players, discardPile, currentPlayer, usedNoteTokens, usedStormTokens):
+        self.tableCards=tableCards
+        self.players=players
+        self.discardPile=discardPile
+        self.currentPlayer=currentPlayer
+        self.usedNoteTokens=usedNoteTokens
+        self.usedStormTokens=usedStormTokens
+
+act_state = state(tableCards,players,discardPile, 'albo', 0, 0)
+
